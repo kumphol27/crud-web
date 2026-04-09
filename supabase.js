8bfb165d3d254cb03dad51fb5177ac87604fbbcb
kumphol27 <134289982+kumphol27@users.noreply.github.com>
2026-04-09 11:46:46 +0700

    supabase.js

--- supabase.js
+++ supabase.js
@@ -1,11 +1,9 @@
 const SUPABASE_URL = "https://cicvdydnrgrgjdpeufuc.supabase.co";
 const SUPABASE_KEY = "sb_publishable_KRMgo0_VGMTqToKk-Ur-yQ_FUFOgXFm";
 
-// 👇 ดึงจาก global object
-const supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
+// ใช้ global จาก CDN
+const db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-// export ไปใช้
-window.supabase = supabaseClient;
+// export ให้ทุกไฟล์ใช้
+window.db = db;
+window.SUPABASE_URL = SUPABASE_URL;
