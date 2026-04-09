9b9ce36bec71d6e9f5858ddde8e25085e715f75a
kumphol27 <134289982+kumphol27@users.noreply.github.com>
2026-04-09 11:35:59 +0700

    supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
-<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js"></script>
-<script>
 const SUPABASE_URL = "https://cicvdydnrgrgjdpeufuc.supabase.co";
 const SUPABASE_KEY = "sb_publishable_KRMgo0_VGMTqToKk-Ur-yQ_FUFOgXFm";
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-</script>
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
